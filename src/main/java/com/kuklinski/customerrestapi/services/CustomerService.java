41a1293d04304633ce3e4fcff7123a91139f7e67
Riczard <riczardkiller@gmail.com>
2019-09-25 17:42:27 +0200

    implement CustomerService

---
+++ src/main/java/com/kuklinski/customerrestapi/services/CustomerService.java
@@ -0,0 +1,23 @@
+package com.kuklinski.customerrestapi.services;
+
+import com.kuklinski.customerrestapi.domain.Customer;
+import com.kuklinski.customerrestapi.repositores.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer getCustomerById(String id) {
+        return customerRepository.getCustomerById(id);
+    }
+
+    public Customer addCustomer(Customer customer) {
+        return customerRepository.addCustomer(customer);
+    }
+}
