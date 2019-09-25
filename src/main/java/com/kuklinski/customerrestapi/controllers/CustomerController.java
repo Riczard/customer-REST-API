0a315ea6179fd8003b8624847120688c9e3487e8
Riczard <riczardkiller@gmail.com>
2019-09-25 17:41:22 +0200

    implement CustomerController

---
+++ src/main/java/com/kuklinski/customerrestapi/controllers/CustomerController.java
@@ -0,0 +1,30 @@
+package com.kuklinski.customerrestapi.controllers;
+
+import com.kuklinski.customerrestapi.domain.Customer;
+import com.kuklinski.customerrestapi.services.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable String id) {
+        return new ResponseEntity<>(customerService.getCustomerById(id), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/customer")
+    public ResponseEntity<Customer> addCustomer(@Valid @RequestBody Customer customer) {
+        return new ResponseEntity<>(customerService.addCustomer(customer), HttpStatus.CREATED);
+    }
+
+}
