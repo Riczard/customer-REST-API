41aa03a614e104aab4c78f46d9cff687a0130904
Riczard <riczardkiller@gmail.com>
2019-09-25 15:43:33 +0200

    add Customer model

---
+++ src/main/java/com/kuklinski/customerrestapi/domain/Customer.java
@@ -0,0 +1,37 @@
+package com.kuklinski.customerrestapi.domain;
+
+public class Customer {
+
+    private long id;
+
+    private String name;
+
+    private Adress adress;
+
+    public Customer() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Adress getAdress() {
+        return adress;
+    }
+
+    public void setAdress(Adress adress) {
+        this.adress = adress;
+    }
+}
